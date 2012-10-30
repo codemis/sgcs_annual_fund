4cf76a49580cd9b30abc01cab176ddc7c80c0c00
Codemis <johnathan@missionaldigerati.com>
2012-10-30 16:51:39 -0700

    Added error handling on donation form.

--- js/application.js
+++ js/application.js
@@ -7,15 +7,17 @@ jQuery(document).ready(function($) {
 		if(link.hasClass('disabled')) {
 			return false;
 		}
-		var donateAmount = $('input#d-amount').val();
-		if((donateAmount == '') || (donateAmount == 'e.g. 10, 20, 100.. ')) {
-			alert('Please specify the donation amount.');
-			return false;
-		} else{
-			link.addClass('disabled');
-			link.children('span:first').text('Processing....');
-			link.parent('form').submit();
-			return false;
+		if($('input#d-amount').length > 0) {
+			var donateAmount = $('input#d-amount').val();
+			if((donateAmount == '') || (donateAmount == 'e.g. 10, 20, 100.. ')) {
+				$('div#donate-error-msg').html('<p>Please provide a donation amount.</p>');
+				$('input#d-amount').addClass('error');
+				return false;
+			}
 		}
+		link.addClass('disabled');
+		link.children('span:first').text('Processing....');
+		link.parent('form').submit();
+		return false;
 	});
 });
